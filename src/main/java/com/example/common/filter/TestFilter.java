c081707d36d6f070e900b9f7c5bf1bd572ccb3f9
lyhans <lyhans@yeah.net>
2018-12-23 22:55:51 +0800

    httpclient

--- src/main/java/com/example/common/filter/TestFilter.java
+++ src/main/java/com/example/common/filter/TestFilter.java
@@ -1,8 +1,15 @@
-package com.example.filter;
+package com.example.common.filter;
+
+import org.springframework.core.annotation.Order;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.util.Map;
 
+@Order(1)
+//重点
+@WebFilter(filterName = "testFilter1", urlPatterns = "/*")
 public class TestFilter implements Filter {
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
@@ -12,6 +19,7 @@ public class TestFilter implements Filter {
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
       throws IOException, ServletException {
+    Map<String, String[]> map = servletRequest.getParameterMap();
     System.out.println("TestFilter");
   }
  
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.common.utils.HttpRequestUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String str = HttpRequestUtil.get("http://apis.juhe.cn/idcard/index?key=5d32b9502a5e290d320736eaa85c4f3c&cardno=330326198903081211");
+		System.out.println(str);
 	}
 
 }
