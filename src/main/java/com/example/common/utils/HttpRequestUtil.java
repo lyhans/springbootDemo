fa1abef9fe997694ebb2712307b7b47c3d66e46a
lyhans <lyhans@yeah.net>
2018-12-25 22:51:14 +0800

    test

--- src/main/java/com/example/common/utils/HttpRequestUtil.java
+++ src/main/java/com/example/common/utils/HttpRequestUtil.java
@@ -8,7 +8,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
- 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
